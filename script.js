46fa4add9218d5bdc3bbfd82c1f09075e5b9cf4d
Pavan Kumar <Pavan.Kumar173@T-Mobile.com>
2022-06-15 18:28:34 +0530

    mobile responsive design-v1

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".header__dashboardlist>ul a");
+const navMenu = document.querySelector(".header__nav-inks");
+let menuBg = document.querySelector(".header__menu-bg");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+  navMenu.classList.toggle("show");
+  menuBg.classList.toggle("show-bg");
+  // console.log(e.target);
 });
