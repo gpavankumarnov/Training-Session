50475a1890a335f0cc69eb67c966067664175ac6
Pavan Kumar <Pavan.Kumar173@T-Mobile.com>
2022-06-16 18:24:52 +0530

    css responsive

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".header__nav-inks");
 let menuBg = document.querySelector(".header__menu-bg");
@@ -7,3 +9,24 @@ hamburger.addEventListener("click", () => {
   menuBg.classList.toggle("show-bg");
   // console.log(e.target);
 });
+
+//Initializaton of sliders
+// let sliderNumber = 0;
+// let maxSlider = 4;
+// let outPut = document.getElementById("output");
+
+// function nextSlide() {
+//   sliderNumber = sliderNumber + 1;
+//   console.log(sliderNumber);
+
+//   // debugger;
+
+//   outPut.src = "./imgs/image" + sliderNumber + ".svg";
+// }
+
+let slider = document.getElementById("slider-main");
+let items = document.getElementsByName("item");
+
+function nextSlide() {
+  slider.append(items[0]);
+}
